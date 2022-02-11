1066db55e6970e8b8cf598631b1ac56cda300a57
nsmchan <ehfhfh1313@naver.com>
2022-02-11 18:20:43 +0900

    [#4] 영화 상세보기, 리뷰, 팁 컨트롤러 추가

---
+++ ott_2/src/main/java/com/winter/ott/controller/reviewController.java
@@ -0,0 +1,38 @@
+package com.winter.ott.controller;
+
+
+import lombok.extern.log4j.Log4j2;          //콘솔출력
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/movies/movie-id/reviews")
+@Log4j2
+public class reviewController {
+
+    @GetMapping("/")
+    public String index() {
+        return "/movies/movie-id/reviews";
+    }
+
+    // 수정을 하고 리뷰페이지로
+    @PatchMapping("/review-id/modify")
+    public String modifyReview(){
+        log.info("리뷰 modify.........................................");
+        return "redirect:/movies/movie-id/reviews";
+    }
+
+    // 삭제를 하고 리뷰 페이지로
+    @DeleteMapping("/tips/remove")
+    public String deleteReview(){
+        log.info("리뷰 delete........................................");
+        return "redirect:/movies/movie-id/reviews";
+    }
+
+    // 포스팅을 하고 리뷰 페이지로
+    @PostMapping("/review-id/post")
+    public String postReview(){
+        log.info("리뷰 post.........................................");
+        return "redirect:/movies/movie-id/reviews";
+    }
+
+}
