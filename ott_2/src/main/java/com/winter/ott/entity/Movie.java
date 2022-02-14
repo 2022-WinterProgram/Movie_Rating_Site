aa8ca326ec250608afc54a83b5d327981ea5a48a
YoungseonHong <hys2037@naver.com>
2022-02-14 18:15:51 +0900

    [#6]검색페이지 백엔드 구현

---
+++ ott_2/src/main/java/com/winter/ott/entity/Movie.java
@@ -0,0 +1,96 @@
+package com.winter.ott.entity;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Getter
+@Setter
+@Entity
+@NoArgsConstructor
+@Table(name = "movie")
+public class Movie {
+
+    @Id
+    @Column(name = "ID")
+    private int id;
+
+    @Column(name = "m_title")
+    private String title;
+
+    @Column(name = "m_Year")
+    private int year;
+
+    @Column(name = "Age", columnDefinition = "TEXT")
+    private String age;
+
+    @Column(name = "IMDb")
+    private double imdb;
+
+    @Column(name = "`RottenTomatoes`")
+    private String rottenTomatoes;
+
+    @Column(name = "Netflix")
+    private int netflix;
+
+    @Column(name = "`Disney+`")
+    private int disney;
+
+    @Column(name = "Directors", columnDefinition = "TEXT")
+    private String directors;
+
+    @Column(name = "Genres2", columnDefinition = "TEXT")
+    private String genres2;
+
+    @Column(name = "Genres3", columnDefinition = "TEXT")
+    private String genres3;
+
+    @Column(name = "Genres4", columnDefinition = "TEXT")
+    private String genres4;
+
+    @Column(name = "Genres5", columnDefinition = "TEXT")
+    private String genres5;
+
+    @Column(name = "Genres6", columnDefinition = "TEXT")
+    private String genres6;
+
+    @Column(name = "Country", columnDefinition = "TEXT")
+    private String country;
+
+    @Column(name = "Language", columnDefinition = "TEXT")
+    private String language;
+
+    @Column(name = "Runtime")
+    private int runtime;
+
+    @Column(name = "`Actor 1`", columnDefinition = "TEXT")
+    private String actor1;
+
+    @Column(name = "`Actor 2`", columnDefinition = "TEXT")
+    private String actor2;
+
+    @Column(name = "`Actor 3`", columnDefinition = "TEXT")
+    private String actor3;
+
+    @Column(name = "`Actor 4`", columnDefinition = "TEXT")
+    private String actor4;
+
+    @Column(name = "Description", columnDefinition = "TEXT")
+    private String description;
+
+    @Column(name = "Poster", columnDefinition = "TEXT")
+    private String poster;
+
+    public Movie(String mTitle, int year, String country, String poster) {
+        this.title = mTitle;
+        this.year = year;
+        this.country = country;
+        this.poster = poster;
+    }
+
+}
---
+++ ott_2/src/main/java/com/winter/ott/repository/MovieRepository.java
@@ -0,0 +1,12 @@
+package com.winter.ott.repository;
+
+import com.winter.ott.entity.Movie;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface MovieRepository extends JpaRepository<Movie, String> {
+
+    List<Movie> findMovieByTitle(String title);
+
+}
