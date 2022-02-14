104c1077268d08355048776b196c8d009d28ff64
nsmchan <ehfhfh1313@naver.com>
2022-02-15 01:29:55 +0900

    [#8] 영화 상세보기, 리뷰, 팁 컨트롤러 수정

--- ott_2/src/main/java/com/winter/ott/controller/detailCotroller.java
+++ ott_2/src/main/java/com/winter/ott/controller/detailCotroller.java
@@ -1,24 +1,74 @@
 package com.winter.ott.controller;
 
+import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+//import com.winter.ott.dto
 
 
 @RestController
-@RequestMapping("/movies/movie-id")
+@RequestMapping("/movies/{movie-id}")
 @Log4j2
+@RequiredArgsConstructor
 public class detailCotroller {
+
+    // 영화 기본 정보 반환
     @GetMapping("/")
     public String index() {
-        return "/movies/movie-id";
+        return "/movies/{movie-id}";
     }
 
+    // 영화 상세 정보
     @GetMapping("/details")
-    public void details(){
-        log.info("상세정보 get...");
+    public void details(){log.info("details get...");}
+
+    /// 평가하기 눌렀을때
+    // 포스트 모달 띄우기
+    @GetMapping("/reviews/{review-id}/post")
+    public void review(){log.info("review post.........................................");}
+
+    // 포스팅을 하고 리뷰 페이지로
+    @PostMapping("/reviews/{review-id}/post")
+    public String postReview(){
+        log.info("review post.........................................");
+        return "redirect:/movies/{movie-id}/reviews";
     }
+
+
+    /// 팁 더보기 눌렀을때
+    // 모달창 띄우기
+    @GetMapping("/tips")
+    public void Tip(){ log.info("팁 post");
+    }
+    //팁 작성
+    @PostMapping("/tips")
+    public String postTip(){
+
+        log.info("팁 post");
+        //
+        // 코드작성
+        //
+        String text="";
+        return "/movies/{movie-id}";
+    }
+
+/* 팁 수정하기
+
+    @PutMapping("/tips/modify")
+    public String modifyTip(){
+
+        log.info("팁 modify");
+        //
+        // 코드작성
+        //
+        String text="";
+        return text;
+
+    }
+
+*/
+
 
 
 }
--- ott_2/src/main/java/com/winter/ott/controller/reviewController.java
+++ ott_2/src/main/java/com/winter/ott/controller/reviewController.java
@@ -1,38 +1,57 @@
 package com.winter.ott.controller;
 
 
-import lombok.extern.log4j.Log4j2;          //콘솔출력
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/movies/movie-id/reviews")
+@RequestMapping("/movies/{movie-id}/reviews")
 @Log4j2
+@RequiredArgsConstructor
 public class reviewController {
 
+
+
+
+    //영화 리뷰 목록
     @GetMapping("/")
     public String index() {
-        return "/movies/movie-id/reviews";
+        return "/movies/{movie-id}/reviews";
     }
+/*
 
-    // 수정을 하고 리뷰페이지로
-    @PatchMapping("/review-id/modify")
-    public String modifyReview(){
-        log.info("리뷰 modify.........................................");
-        return "redirect:/movies/movie-id/reviews";
-    }
-
-    // 삭제를 하고 리뷰 페이지로
-    @DeleteMapping("/tips/remove")
-    public String deleteReview(){
-        log.info("리뷰 delete........................................");
-        return "redirect:/movies/movie-id/reviews";
-    }
+    /// 영화 리뷰 목록에서 내 리뷰를 클릭했을때
+    // 포스트 모달 띄우기
+    @GetMapping("/{review-id}/post")
+    public void post(){log.info("review post.........................................");}
 
     // 포스팅을 하고 리뷰 페이지로
-    @PostMapping("/review-id/post")
+    @PostMapping("/{review-id}/post")
     public String postReview(){
-        log.info("리뷰 post.........................................");
-        return "redirect:/movies/movie-id/reviews";
+        log.info("review post.........................................");
+        return "redirect:/movies/{movie-id}/reviews";
     }
+*/
+
+/*
+수정 기능 보류
+
+    // 수정을 하고 리뷰페이지로
+    @PatchMapping("/{review-id}/modify")
+    public String modifyReview(){
+        log.info("review modify.........................................");
+        return "redirect:/movies/{movie-id}/reviews";
+    }
+*/
+
+    // 삭제를 하고 리뷰 페이지로
+    @DeleteMapping("/remove")
+    public String deleteReview(){
+        log.info("review delete........................................");
+        return "redirect:/movies/{movie-id}/reviews";
+    }
+
+
 
 }
