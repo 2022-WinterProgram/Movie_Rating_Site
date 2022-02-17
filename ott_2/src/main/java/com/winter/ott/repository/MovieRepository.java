627bd180b2a477f13f9389cc03bb707deafecab7
YoungseonHong <47203158+YoungseonHong@users.noreply.github.com>
2022-02-17 15:25:48 +0900

    search repository수정

--- ott_2/src/main/java/com/winter/ott/repository/MovieRepository.java
+++ ott_2/src/main/java/com/winter/ott/repository/MovieRepository.java
@@ -6,11 +6,16 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface MovieRepository extends JpaRepository<Movie, String> {
+    List<Movie> findMovieByActor1(String actor);
+    List<Movie> findMovieByActor2(String actor);
+    List<Movie> findMovieByActor3(String actor);
+    List<Movie> findMovieByActor4(String actor);
 
     List<Movie> findMovieByTitle(String title);
 
-    List<Movie> findMovieByActor(String actor);
-
-    List<Movie> findMovieByDirector(String director);
+    //search에 필요함
+    List<Movie> findAllByTitle(String title);
+    List<Movie> findAllByActor(String actor);
+    List<Movie> findAllByDirector(String director);
 
 }
