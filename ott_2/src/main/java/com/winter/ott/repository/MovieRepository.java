8e01028382183f7ba313e174d4c6ae3ad00e0afc
YoungseonHong <47203158+YoungseonHong@users.noreply.github.com>
2022-02-17 14:56:03 +0900

    Add files via upload

--- ott_2/src/main/java/com/winter/ott/repository/MovieRepository.java
+++ ott_2/src/main/java/com/winter/ott/repository/MovieRepository.java
@@ -1,36 +1,16 @@
 package com.winter.ott.repository;
 
-import com.winter.ott.entity.Actor;
 import com.winter.ott.entity.Movie;
-
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 
-import org.springframework.data.domain.Pageable;
 import java.util.List;
 
 public interface MovieRepository extends JpaRepository<Movie, String> {
-    //search
+
     List<Movie> findMovieByTitle(String title);
-    //main
-    List<Movie> findMovieByActor1(String name);
-    List<Movie> findMovieByActor2(String name);
-    List<Movie> findMovieByActor3(String name);
-    List<Movie> findMovieByActor4(String name);
 
-    List<Movie> findMovieByGenres1(String genre);
-    List<Movie> findMovieByGenres2(String genre);
-    List<Movie> findMovieByGenres3(String genre);
-    List<Movie> findMovieByGenres4(String genre);
-    List<Movie> findMovieByGenres5(String genre);
-    List<Movie> findMovieByGenres6(String genre);
+    List<Movie> findMovieByActor(String actor);
 
-    @Query("select m from Movie m")
-    List<Movie> findIMDbTop10(Pageable paging);
-
-//    List<Movie> findMovieByImdb(double score);
-//    List<Movie> findMovieByDisney(int is_diseny);
-//    List<Movie> findMovieByNetflix(int is_netflix);
-//    List<Movie> findMovieByGenre2(String genre);
+    List<Movie> findMovieByDirector(String director);
 
 }
